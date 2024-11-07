344b8bd3a1bf2b68ba164a147e35e53733b8d920
Victor Hugo <161778672+VitinDuCray@users.noreply.github.com>
2024-11-07 20:57:08 -0300

    Add files via upload

---
+++ src/components/ListaDeContatos.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function ListaDeContatos() {
+    const [contatos, setContatos] = useState([]);
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] =  useState('');
+
+    const adicionarContato =  () => {
+        if(nome && telefone) {
+            setContatos([...contatos, {nome, telefone}]);
+            setNome('');
+            setTelefone('');
+        }
+    };
+
+    return (
+        <>
+        <h2>Lista de Contatos</h2>
+        <div>
+            <input 
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Nome"
+            />
+            <input 
+            type="text"
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+            placeholder="Telefone"
+            />
+            <button onClick={adicionarContato}>Adicionar</button>
+        </div>
+        <ul>
+            {contatos.map((contato,index) => (
+                <li key={index}>
+                    <strong>{contato.nome}:</strong> {contato.telefone}
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+
+}
+
+export default ListaDeContatos
